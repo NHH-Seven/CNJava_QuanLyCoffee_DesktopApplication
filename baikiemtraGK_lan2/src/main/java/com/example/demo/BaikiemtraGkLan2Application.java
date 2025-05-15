be1b95ed7a4e5f8f3760e16cab561d42d3168ee9
NHH-Seven <nghuyhieu3010@gmail.com>
2025-05-15 14:14:57 +0700

    bai kiem tra giua ki lan 2 ( dang nhap & dang ky)

---
+++ baikiemtraGK_lan2/src/main/java/com/example/demo/BaikiemtraGkLan2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaikiemtraGkLan2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaikiemtraGkLan2Application.class, args);
+	}
+
+}
---
+++ baikiemtraGK_lan2/src/main/java/com/example/demo/LoginController.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class LoginController {
+	@GetMapping("/Welcome")
+	public String showWelcomePage() {
+	    return "welcome"; // trả về file welcome.html
+	}
+    @GetMapping("/login")
+    public String showLoginForm(Model model) {
+        return "login"; // trả về login.html
+    }
+
+    @PostMapping("/login")
+    public String processLogin(@RequestParam String username,
+                               @RequestParam String password,
+                               Model model) {
+        if ("admin".equals(username) && "123".equals(password)) {
+            model.addAttribute("message", "Đăng nhập thành công!");
+        } else {
+            model.addAttribute("message", "Sai tên đăng nhập hoặc mật khẩu!");
+        }
+        return "login";
+    }
+}
